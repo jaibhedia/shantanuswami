fb5dfd3fd54bb93713e66ecde4ad93ddfc994f3b
Shantanu Swami <57904885+jaibhedia@users.noreply.github.com>
2023-10-24 13:15:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: { manifest: true, outDir: './dist'},
+  base:  '/'
   plugins: [react()],
 })
